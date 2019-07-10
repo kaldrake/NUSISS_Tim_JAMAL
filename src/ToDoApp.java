9ec7878a9b7d877b70c59a12bc3b745869f383ff
Wesley Chan (NCS) <wesley.chan@ncs.com.sg>
2019-07-10 14:46:55 +0800

    Adding app stuff

---
+++ src/ToDoApp.java
@@ -0,0 +1,14 @@
+
+public class ToDoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		displayMenu();
+	}
+	
+	public static void displayMenu() {
+		System.out.println("Welcome to ToDo-List Application ============\n");
+		System.out.println("a) Add task\nb) Remove task\nc) Display all tasks\nd) Update task");
+		System.out.println("\nPlease enter your choice: ");
+	}
+}
